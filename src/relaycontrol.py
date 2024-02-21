ea1bb4c92ace7411193378da334bfbf2bdd51ff6
ak <awkk12345@gmail.com>
2024-02-21 11:01:46 +0200

    Small update to  commentary

--- src/relaycontrol.py
+++ src/relaycontrol.py
@@ -6,20 +6,26 @@ import time
 
 def RelayControl(on:bool):
 
+    '''
+    Control the state of a relay connected to a Raspberry Pi GPIO pin.
+
+    Args:
+    on (bool): True to turn the relay on, False to turn it off.
+    '''
 
     gpio_pin = 27
-
-
     GPIO.setmode(GPIO.BCM)
 
 
 
     if on == True:
-        GPIO.setup(gpio_pin, GPIO.OUT) #gpio pinni päälle
+        GPIO.setup(gpio_pin, GPIO.OUT) #turn on gpio
+
 
     elif on == False:
 
-        GPIO.setup(gpio_pin,GPIO.IN) #gpio pinni pois päältä
+        GPIO.setup(gpio_pin,GPIO.IN) #turn off gpio
+
 
 
 
